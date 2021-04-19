830ee8709112a93ae663bdef1c776fc0a893dc50
veronika_sol <soverick@hotmail.com>
2021-04-19 19:25:01 +0300

    random taskt to views.py added

--- 01_IntroductionToWebFrameworks/todo/tasks/views.py
+++ 01_IntroductionToWebFrameworks/todo/tasks/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponse
 
 from django.views import View
+import random
 
 
 class ToDoView(View):
 
     def get(self, request, *args, **kwargs):
-        return HttpResponse('<ul>'
-                            '<li>Установить python</li>'
-                            '<li>Установить django</li>'
-                            '<li>Запустить сервер</li>'
-                            '<li>Порадоваться результату</li>'
-                            '</ul>')
+    	elements = ['Помыть посуду',' Убрать со стола', 'Покормить кота',
+    	'Погулять', 'Поработать', 'Послушать подкаст', 'Почитать газету', 
+    	'Установить python', 'Установить django', 'Запустить сервер', 
+    	'Порадоваться результату', 'Порадоваться новому дню']
+    	already_in = []
+    	to_do_list = '<ul>'
+    	for i in range(5):
+    		choice = random.randint(0,len(elements)-1)
+    		while choice in already_in:
+    			choice = random.randint(0,len(elements)-1)
+    		to_do_list += '<li>' + elements[choice] + '</li>'
+    		already_in.append(choice)
+    	to_do_list += '</ul>'
+    	return HttpResponse(to_do_list)
