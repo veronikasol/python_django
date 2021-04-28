89e458c6100fbca61d0d153635fac3adaf277b3d
veronika_sol <soverick@hotmail.com>
2021-04-28 21:03:54 +0300

    deleted my own folder 05_Forms because there is a skillbox folder

--- 05_Forms/news_site/news_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from news_app.views import NewsFormView
-
-urlpatterns = [
-path('', views.NewsListView.as_view(), name='news'),
-path('<int:news_id>', views.news_detail, name='news_detail'),
-path('news', NewsFormView.as_view(), name='news_form')
-]
