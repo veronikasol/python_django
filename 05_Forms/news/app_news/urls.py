134ccfaca30e25a1827635e56eb1d4db26fe1e06
veronika_sol <soverick@hotmail.com>
2021-04-28 21:19:30 +0300

    models(News, Comment) , forms for news and comment addition, templates for news_list, news_detail (with comment addition form) and news_creation form added

--- 05_Forms/news/app_news/urls.py
+++ 05_Forms/news/app_news/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from . import views
+from app_news.views import NewsFormView
 
 urlpatterns = [
-
+path('', views.NewsListView.as_view(), name='news'),
+path('<int:news_id>', views.news_detail, name='news_detail'),
+path('news', NewsFormView.as_view(), name='news_form')
 ]
--- 05_Forms/news/app_news/views.py
+++ 05_Forms/news/app_news/views.py
@@ -1,1 +1,44 @@
 from django.shortcuts import render
+
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.views.generic.list import ListView
+from django.views import View
+from app_news.models import News, Comment
+from app_news.forms import CommentForm, NewsForm
+
+class NewsListView(ListView):
+	model = News
+	template_name = 'news_list.html'
+	context_object_name = 'news_list'
+
+
+def news_detail(request, news_id):
+	news = News.objects.get(pk=news_id) # метод get y News
+	comments = news.comments.all()
+	new_comment = None
+	if request.method == 'POST':
+		comment_form = CommentForm(data=request.POST)
+		if comment_form.is_valid():
+			new_comment = comment_form.save(commit=False)
+			new_comment.news = news
+			new_comment.save()
+	else:
+		comment_form = CommentForm()
+
+	return render(request,'app_news/news_detail.html', {'news':news, 'comments':comments,
+		'new_comment':new_comment,'comment_form':comment_form})
+
+
+class NewsFormView(View):
+	
+	def get(self,request):
+		news_form = NewsForm()
+		return render(request,'app_news/news_form.html', context={'news_form':news_form})
+
+	def post(self, request):
+		news_form = NewsForm(request.POST)
+		if news_form.is_valid():
+			News.objects.create(**news_form.cleaned_data)
+			return HttpResponseRedirect('/')
+		return render(request, 'app_news/news_form.html', context={'news_form':news_form})
