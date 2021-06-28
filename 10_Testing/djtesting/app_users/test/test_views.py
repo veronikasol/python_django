73646314be08dacf522ffd5ea392a49a111a9ea3
veronika_sol <soverick@hotmail.com>
2021-06-28 09:54:25 +0300

    test for registration page to check proper view function is used, page is responding and has registration title

---
+++ 10_Testing/djtesting/app_users/test/test_views.py
@@ -0,0 +1,29 @@
+from django.test import TestCase
+from ..views import register_view
+
+# Тестирование приложения для учета пользователей
+"""Пользователи могут 
+	незарегистрированные - зарегистрироваться на сайте
+	зарегистрированные:
+		залогиниться на сайте
+		смотреть свой профиль
+		редактировать свой профиль
+"""
+
+class UserRegistrationTest(TestCase):
+	"""Может ли пользователь зарегистрироваться на сайте указав имя и пароль:
+	По адресу register 
+	пользователь видит форму для регистрации
+	Вводит свои данные 
+	и при успешной регистрации перенаправляется на главную страницу 
+	как аутентифицированный и залогиненный
+	"""
+
+	def test_proper_register_view(self):
+		response = self.client.get('/user/register')
+		self.assertEqual(response.resolver_match.func, register_view)
+		self.assertEqual(response.status_code, 200)
+		self.assertContains(response, 'Регистрация')
+
+	#def test_can_post_username_and_password(self):
+		#response = self.client.post('register', )
--- 10_Testing/djtesting/app_users/views.py
+++ 10_Testing/djtesting/app_users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register_view(request):
+	form = {}
+	return render(request,'app_users/register.html', {'form':form})
