51c664af55400bfc84d76223286a8bc6afdb8944
veronika_sol <soverick@hotmail.com>
2021-06-29 14:30:30 +0300

    Test for post forms together with template added

--- 10_Testing/djtesting/app_blog/test/test_forms.py
+++ 10_Testing/djtesting/app_blog/test/test_forms.py
@@ -1,3 +1,25 @@
 from django.test import TestCase, RequestFactory
 from django.urls import reverse
 from django.contrib.auth.models import User, AnonymousUser
+from ..forms import PostForm, MultiFileForm
+
+class PostFormTest(TestCase):
+	def setUp(self):
+		self.user = User.objects.create_user(username='test_12')
+		self.user.set_password('secret_12A')
+		self.user.save()
+		self.client.login(username='test_12', password='secret_12A')
+
+
+	def test_proper_post_form(self):
+		response = self.client.get('/posts/')
+		self.assertIsInstance(response.context['post_form'], PostForm)
+		self.assertIsInstance(response.context['file_form'], MultiFileForm)
+
+	def test_post_form_renders(self):
+		form_bad = PostForm(data={})
+		self.assertFalse(form_bad.is_valid())
+		form_good = PostForm(data={'title':'test_1', 'content':'secret_1A'})
+		file_good = MultiFileForm(data={'file':'kjdfkjdkfdkf.jdk'})
+		self.assertTrue(form_good.is_valid())
+		self.assertTrue(file_good.is_valid())
--- 10_Testing/djtesting/app_blog/views.py
+++ 10_Testing/djtesting/app_blog/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import PostForm, MultiFileForm
+
+
 
 # Create your views here.
 def posts_view(request):
 	
 	return render(request,'app_blog/posts.html')
+
+
+class PostCreateView(View, LoginRequiredMixin):
+	
+	def get(self,request):
+		post_form = PostForm()
+		file_form = MultiFileForm()
+		return render(request,'app_blog/post_form.html', context={'post_form':post_form, 'file_form':file_form})
+
+	def post(self, request):
+		post_form = PostForm(request.POST)
+		file_form = MultiFileForm(request.POST, request.FILES)
+		if post_form.is_valid():
+			cd = post_form.cleaned_data
+			new_post = post_form.save(commit=False)
+			new_post.user = request.user
+			new_post.save()
+			if file_form.is_valid():
+				files = request.FILES.getlist('file_field')
+				for f in files:
+					instance = File(file=f, post=new_post)
+					instance.save()
+			return HttpResponseRedirect('/')
+		return render(request, 'app_blog/post_form.html', context={'post_form':post_form, 'file_form':file_form})
