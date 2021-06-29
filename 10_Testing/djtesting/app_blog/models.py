75c00c9788becb147d279293e7be8061e527f98c
veronika_sol <soverick@hotmail.com>
2021-06-29 13:46:30 +0300

    test folder for app_bog added with preliminary action plan, models for app_blog and foreignkey for post to the profile model added

--- 10_Testing/djtesting/app_blog/models.py
+++ 10_Testing/djtesting/app_blog/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class File(models.Model):
+	file = models.FileField(blank=True, upload_to='files/', verbose_name='Файлы')
+	description = models.TextField(blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+	post = models.ForeignKey('Post', default=None, null=True,
+		on_delete=models.CASCADE, related_name='files', verbose_name='публикация')
+
+	def __str__(self):
+		return f'{self.file}'
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=1000, verbose_name='Заголовок')
+	content = models.TextField(verbose_name='Текст публикации')
+	created_at = models.DateTimeField(auto_now_add=True, verbose_name='дата создания')
+	modified_at = models.DateTimeField(auto_now=True, verbose_name='дата редактирования')
+	user = models.ForeignKey(User, default=None, null=True,
+		on_delete=models.CASCADE, related_name='post')
+
+	def __str__(self):
+		return f'{self.title} от {self.created_at.date()} ({self.user.username})'
+
+	class Meta:
+		db_table = 'Post'
+		ordering = ['-created_at',]
---
+++ 10_Testing/djtesting/app_blog/test/test_views.py
@@ -0,0 +1,3 @@
+from django.test import TestCase, RequestFactory
+from django.urls import reverse
+from django.contrib.auth.models import User, AnonymousUser
